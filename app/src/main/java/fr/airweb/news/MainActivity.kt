be83f3afb640daf18db07bb368d18a1bf26406c6
Pierre Ghazal <pg.ghazal@gmail.com>
2021-01-20 21:49:12 +0100

    First implementation of NewsViewModel which makes api call to fetch News

--- app/src/main/java/fr/airweb/news/MainActivity.kt
+++ app/src/main/java/fr/airweb/news/MainActivity.kt
@@ -1,11 +1,18 @@
 package fr.airweb.news
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import fr.airweb.news.viewmodel.NewsViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val newsViewModel: NewsViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        newsViewModel.fetchNewsRemote()
     }
 }
